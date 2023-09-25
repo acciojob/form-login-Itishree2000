4c94fbb3d23f434208d66e4d181e38235eec6620
Itishree2000 <127124225+Itishree2000@users.noreply.github.com>
2023-09-25 16:47:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function getFormvalue() {
     //Write your code here
+ const form = document.getElementById("form1");
+
+    // Get the values of the first name and last name input fields
+    const firstNameInput = form.elements.fname;
+    const lastNameInput = form.elements.lname;
+
+    // Get the values from the input fields
+    const firstName = firstNameInput.value;
+    const lastName = lastNameInput.value;
+
+    // Check if both fields are filled
+    if (firstName.trim() === "" || lastName.trim() === "") {
+        alert("Please fill in both first name and last name fields.");
+    } else {
+        // Display the first and last names using alert
+        alert("First Name: " + firstName + "\nLast Name: " + lastName);
+    }
+
+    // Prevent the form from submitting and refreshing the page
+    return false;
 
 }
